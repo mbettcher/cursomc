5f1cfd2dee00ca918adf80247a4344caa89f6782
mbettcher <mtonon.pmg@gmail.com>
2021-10-12 17:04:00 -0300

    Criando repository e service para Categoria

---
+++ src/main/java/br/com/mtonon/cursomc/repositories/CategoriaRepository.java
@@ -0,0 +1,11 @@
+package br.com.mtonon.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import br.com.mtonon.cursomc.domain.Categoria;
+
+@Repository
+public interface CategoriaRepository extends JpaRepository<Categoria, Integer>{
+
+}
