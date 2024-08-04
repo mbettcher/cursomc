ba45d4552020f29c8eeb6c5b56f3b690f96d122c
mbettcher <mtonon.pmg@gmail.com>
2024-08-04 18:25:12 -0300

    Implementando validação personalizada EMAIL VALIDATOR
    1 - Implementa validação email não repetido na inserção de um Cliente.

--- src/main/java/br/com/cdm/cursomc/repositories/ClienteRepository.java
+++ src/main/java/br/com/cdm/cursomc/repositories/ClienteRepository.java
@@ -1,9 +1,13 @@
 package br.com.cdm.cursomc.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.transaction.annotation.Transactional;
 
 import br.com.cdm.cursomc.domain.Cliente;
 
 public interface ClienteRepository extends JpaRepository<Cliente, Long> {
 
+	@Transactional(readOnly = true)
+	Cliente findByEmail(String email);
+	
 }
--- src/main/java/br/com/cdm/cursomc/services/validation/ClienteInsertValidator.java
+++ src/main/java/br/com/cdm/cursomc/services/validation/ClienteInsertValidator.java
@@ -3,14 +3,21 @@ package br.com.cdm.cursomc.services.validation;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import br.com.cdm.cursomc.domain.Cliente;
 import br.com.cdm.cursomc.domain.enums.TipoCliente;
 import br.com.cdm.cursomc.dto.ClienteNewDTO;
+import br.com.cdm.cursomc.repositories.ClienteRepository;
 import br.com.cdm.cursomc.resources.exceptions.FieldMessage;
 import br.com.cdm.cursomc.services.validation.utils.BR;
 import jakarta.validation.ConstraintValidator;
 import jakarta.validation.ConstraintValidatorContext;
 
 public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert, ClienteNewDTO> {
+	
+	@Autowired
+	private ClienteRepository clienteRepository;
 
 	@Override
 	public void initialize(ClienteInsert ann) {
@@ -28,6 +35,12 @@ public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert
 		
 		if(objDTO.getTipo().equals(TipoCliente.PESSOAJURIDICA.getCodigo()) && !BR.isValidCNPJ(objDTO.getCpfCnpj())) {
 			list.add(new FieldMessage("cpfOuCnpj","CPF inválido!"));
+		}
+		
+		Cliente auxiliar = clienteRepository.findByEmail(objDTO.getEmail());
+		
+		if(auxiliar != null) {
+			list.add(new FieldMessage("email","Email já cadastrado!"));
 		}
 		
 		
